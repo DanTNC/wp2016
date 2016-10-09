0675acc203508e9b4cd8af3d68a6cb623a0d8d10
DanTNC <fad11204@yahoo.com.tw>
2016-10-09 22:05:01 +0800

    Update try.js

--- try.js
+++ try.js
@@ -4,7 +4,7 @@ bta.addEventListener("click", displayAbout);
 btc.addEventListener("click", displayContact);*/
 $(document).ready(function(){
   $(".a").click(function(){
-    $("img").animate({top: '+=40%'},"slow",function(){
+    $("#image").animate({top: '+=40%'},"slow",function(){
      $("pre").fadeIn("slow");
     });
   });
