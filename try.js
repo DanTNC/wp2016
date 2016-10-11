f8744a0bc6dd2db9a98d54d56bec6144318dc379
DanTNC <fad11204@yahoo.com.tw>
2016-10-11 16:51:51 +0800

    Update try.js

--- try.js
+++ try.js
@@ -1,9 +1,23 @@
-/*var bta=document.getElementById("a");
-var btc=document.getElementById("c");
-bta.addEventListener("click", displayAbout);
-btc.addEventListener("click", displayContact);*/
+/*$(document).ready(function(){
+  $(".a").click(function(){
+    $("#about").slideToggle("slow");
+  });
+});*/
+var state = 0;
 $(document).ready(function(){
   $(".a").click(function(){
     $("#about").slideToggle("slow");
+    state = (state!=0?0:1);
+    alert(state);
+  });
+});
+$(document).ready(function(){
+  $(".c").click(function(){
+    if(state==1){
+      $("#about").slideToggle("slow");
+    }
+    //$("#about").slideToggle("slow");
+    state = (state!=0?0:2);
+    alert(state);
   });
 });
